9348de761dc521c7ef794bd14e4ec2d164006768
Vũ Đình Hưng <hungvd23031998@gmail.com>
2024-05-14 19:23:29 +0700

    CRUD API HoaDon, HoaDonChiTiet

---
+++ HN120_ShopQuanAo.API/Service/Services/ChiTietHoaDonService.cs
@@ -0,0 +1,63 @@
+﻿using HN120_ShopQuanAo.API.Repository.IRepositories;
+using HN120_ShopQuanAo.API.Service.IServices;
+using HN120_ShopQuanAo.Data.Models;
+
+namespace HN120_ShopQuanAo.API.Service.Services
+{
+    public class ChiTietHoaDonService : IChiTietHoaDonService
+    {
+        private readonly IChiTietHoaDonRepository _repo;
+        public ChiTietHoaDonService(IChiTietHoaDonRepository repo)
+        {
+                _repo = repo;
+        }
+        public void CreateCTHD(HoaDonChiTiet hdct)
+        {
+            _repo.CreateCTHD(hdct);
+        }
+
+        public void DeleteCTHD(string ma)
+        {
+            try
+            {
+                _repo.DeleteCTHD(ma);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<HoaDonChiTiet> GetAllHoaDonChiTiet()
+        {
+           return _repo.GetAllHoaDonChiTiet();
+        }
+
+        public HoaDonChiTiet GetHoaDonChiTietByMa(string ma)
+        {
+            try
+            {
+                return _repo.GetHoaDonChiTietByMa(ma);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void UpdateCTHD(HoaDonChiTiet hdct)
+        {
+            try
+            {
+                _repo.UpdateCTHD(hdct);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
