ecda98ff04616ec46b22150aae7735c71ff9ca25
Toannk <toannkph27888@fpt.edu.vn>
2024-06-14 00:06:36 +0700

    Login View

---
+++ HN120_ShopQuanAo.View/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HN120_ShopQuanAo.View.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
