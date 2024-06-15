99768d562cdedfd3fc055ca7916ba6036961bb6c
Vũ Đình Hưng <hungvd23031998@gmail.com>
2024-06-15 12:59:10 +0700

    a

---
+++ HN120_ShopQuanAo.API/IResponsitories/IVoucherRepository.cs
@@ -0,0 +1,13 @@
+﻿using HN120_ShopQuanAo.Data.Models;
+
+namespace HN120_ShopQuanAo.API.IResponsitories
+{
+    public interface IVoucherRepository
+    {
+        IEnumerable<Voucher> GetAllVoucher();
+        Voucher GetVoucherByMa(string ma);
+        void CreateVoucher(Voucher vc);
+        void UpdateVoucher(Voucher vc);
+        void DeleteVoucher(string ma);
+    }
+}
