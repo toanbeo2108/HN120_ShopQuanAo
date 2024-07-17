c067bc3d97718c532625ac2901a882c5b6b21598
Vũ Đình Hưng <hungvd23031998@gmail.com>
2024-07-17 20:27:29 +0700

    update 17/7/2024

--- HN120_ShopQuanAo.View/Areas/Admin/Data/HoaDonWithDetailsViewModel.cs
+++ HN120_ShopQuanAo.View/Areas/Admin/Data/HoaDonWithDetailsViewModel.cs
@@ -4,7 +4,26 @@ namespace HN120_ShopQuanAo.View.Areas.Admin.Data
 {
     public class HoaDonWithDetailsViewModel
     {
-        public HoaDon HoaDon { get; set; }
-        public List<HoaDonChiTiet> HoaDonChiTiets { get; set; }
+        public string MaHoaDon_ { get; set; }
+        public string? tennhanhvien { get; set; }
+        public string? IDkh { get; set; }
+        public string? MaVC { get; set; }
+        public DateTime? NgayTao { get; set; }
+        public DateTime? Ngayupdate { get; set; }
+        public string? tenkhachhang { get; set; }
+        public string? sdtkhhang { get; set; }
+        public string? sdtnhahang { get; set; }
+        public decimal? PhiShip_ { get; set; }
+        public decimal? Tonggiatri { get; set; }
+        public int? tt { get; set; }
+        public string? phanloai { get; set; }
+        public string? tinhthanh_ { get; set; }
+        public string? quanhuyen { get; set; }
+        public string? xaphuong { get; set; }
+        public string? cuthe { get; set; }
+        public string? ghichu { get; set; }
+        public int? pttt { get; set; }
+        //địa chỉ
+       
     }
 }
