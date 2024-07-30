ab4cfd7c4abed42d0253b1aeb8ac3b1c4dd53793
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2024-07-30 13:55:44 +0700

    hiep update code lay vouche cho nguoi dung

--- HN120_ShopQuanAo.View/Areas/Customer/Controllers/VoucherController.cs
+++ HN120_ShopQuanAo.View/Areas/Customer/Controllers/VoucherController.cs
@@ -1,4 +1,5 @@
 ﻿using HN120_ShopQuanAo.Data.Models;
+using HN120_ShopQuanAo.View.Areas.Customer.Data;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
@@ -63,10 +64,37 @@ namespace HN120_ShopQuanAo.View.Areas.Customer.Controllers
                 return View(new List<Voucher>());
             }
         }
+        [HttpGet]
         [HttpPost]
         public async Task<IActionResult> layvouchervekho(string mavc)
         {
             var maND = Request.Cookies["UserId"];
+
+
+            var urlvc = $"https://localhost:7197/GetAllVoucher";
+            var responseVC = await _httpClient.GetAsync(urlvc);
+            string apiurlVC = await responseVC.Content.ReadAsStringAsync();
+            var lstVC = JsonConvert.DeserializeObject<List<Voucher>>(apiurlVC);
+
+            var urlUVC = $"https://localhost:7197/api/Voucher_User/GetVoucher_UserbyUserId/{maND}";
+            var responseUVC = await _httpClient.GetAsync(urlUVC);
+            string apiurlUVC = await responseUVC.Content.ReadAsStringAsync();
+            var lstUVC = JsonConvert.DeserializeObject<List<User_Voucher>>(apiurlUVC);
+            var joinDataUVC = from vc in lstVC
+                              join uvc in lstUVC on vc.MaVoucher equals uvc.MaVoucher
+                              select new VoucherUserView
+                              {
+                                  UserVoucherId = uvc.UserVoucherID,
+                                  UserId = maND,
+                                  MaVoucher = vc.MaVoucher,
+                                  TenVoucher = vc.Ten,
+                                  DonGiaToiThieu = vc.GiaGiamToiThieu,
+                                  GiamGiaToiDa = vc.GiaGiamToiDa,
+                                  GiaTriGiam = vc.GiaTriGiam,
+                                  TrangThai = uvc.TrangThai
+                              };
+            ViewBag.lstUVC = joinDataUVC.ToList();
+
             var urlcreateUVC = $"https://localhost:7197/api/Voucher_User/CreateUVC?userid={maND}&mavc={mavc}";
             var content = new StringContent("Thêm thành công", Encoding.UTF8, "application/json");
             var respon = await _httpClient.PostAsync(urlcreateUVC, content);
