7fa72d515d5d582eebc83270516bdddc3bb80216
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2024-08-09 19:13:40 +0700

    hiep up code ls hoa don voi search

---
+++ HN120_ShopQuanAo.View/wwwroot/js/locsp.js
@@ -0,0 +1,12 @@
+﻿function searchProduct() {
+    var searchQuery = document.getElementById("searchInput").value;
+
+    fetch(`/CustomerHome/GianHangNguoiDung?query=${encodeURIComponent(searchQuery)}`)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("searchResults").innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
